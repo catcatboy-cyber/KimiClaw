99bd2c0690975df33f298e3783f2dd1cffce09b3
catcatboy-cyber <catcatboy@gmail.com>
2026-04-17 17:45:55 +0800

    fix: 补充缺失的 android.util.Log import，修复构建失败

--- app/src/main/java/com/kimiclaw/pet/FloatingLobsterService.java
+++ app/src/main/java/com/kimiclaw/pet/FloatingLobsterService.java
@@ -24,6 +24,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.util.Log;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.FrameLayout;
